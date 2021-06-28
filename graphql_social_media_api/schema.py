1d9ee46b07491f045ecc3aa6ae9984fbb00a4ec5
AlexBulavin <bulavin1970@icloud.com>
2021-06-28 22:45:11 +0300

    Add Inputs, Payloads and Mutations tests

--- graphql_social_media_api/schema.py
+++ graphql_social_media_api/schema.py
@@ -7,6 +7,28 @@ class User(DjangoObjectType):
     class Meta:
         model = models.User
 
+class UserInput(graphene.InputObjectType):
+    name = graphene.String()
+
+class CreateUser(graphene.Mutation): #Создали мутатора mutator
+    class Arguments:
+        input = UserInput(required=True)
+
+    ok = graphene.Boolean()
+    user = graphene.Field(User)
+
+    @staticmethod
+    def mutate(root, info, input):
+        isinstance = models.User(name=input.name)
+        try:
+            isinstance.save()
+        except Exception:
+            return CreateUser(ok=False, user=None)
+
+        #Добавляем пустых followers
+#        isinstance.followers.set([])
+        return CreateUser(ok=True, user=isinstance)
+
 class Query(graphene.ObjectType):
     user = graphene.Field(User, id=graphene.Int())
 
@@ -17,4 +39,8 @@ class Query(graphene.ObjectType):
             return models.User.objects.get(pk=id)
         return None
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
